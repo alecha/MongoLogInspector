23ffce5097221f4b593b19d08274a5ab129a2dbe
Alessio <alessio.cavaleri@gmail.com>
2014-07-16 21:53:35 +0200

    css class on table rows

--- public/js/controllers/LogCtrl.js
+++ public/js/controllers/LogCtrl.js
@@ -2,12 +2,21 @@ angular.module('LogCtrl', []).controller('LogController', function ($scope, LogS
     $scope.logs = LogService.logs;
     $scope.logsProperties = LogService.logsProperties;
 
-    $scope.visibleLogProperties = ["applicationName", "short_message", "state", "timestamp"];
+    $scope.visibleLogProperties = ["applicationName", "host", "level", "short_message", "state", "timestamp"];
 
     $scope.toggleLogProperty = function (logProp) {
         if (_.contains($scope.visibleLogProperties, logProp))
             $scope.visibleLogProperties.splice($scope.visibleLogProperties.indexOf(logProp), 1);
         else
             $scope.visibleLogProperties.push(logProp);
+    }
+
+    $scope.setClass = function(log){
+        switch(log.level){
+            case 1,2,3:
+                return "danger";
+            case 4,5,6:
+                return "info";
+        }
     }
 });
